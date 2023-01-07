61a71fbe45aa4631b86c128baa3ff38e8cd6a975
Omar Majzoub <omarmajzoub1@hotmail.com>
2023-01-07 17:29:58 +0200

    MongoDB Atlas code

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const { MongoClient } = require("mongodb");
+// Replace the uri string with your connection string.
+const uri = `mongodb+srv://Cluster01997:${process.env.CLUSTERPASS}@cluster01997.t9p0rm3.mongodb.net/?retryWrites=true&w=majority`;
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    const database = client.db("sample_mflix");
+    const movies = database.collection("movies");
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: "Back to the Future" };
+    const movie = await movies.findOne(query);
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => console.log(`Listening on port ${port}...`));
